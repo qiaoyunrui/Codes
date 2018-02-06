20ddcf14434984d4f8fc36df6f963bc413d0ef57
Juhezi <juhezix@163.com>
2018-02-06 06:27:52 -0800

    [Update]

---
+++ remdiv.s
@@ -0,0 +1,19 @@
+	.file	"remdiv.c"
+	.text
+	.globl	remdiv
+	.type	remdiv, @function
+remdiv:
+.LFB0:
+	.cfi_startproc
+	movq	%rdi, %rax
+	movq	%rdx, %rdi
+	cqto
+	idivq	%rsi
+	movq	%rax, (%rdi)
+	movq	%rdx, (%rcx)
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	remdiv, .-remdiv
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
